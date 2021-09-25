8c291d2d3da4a4a304979e9bbc84855bf920230d
Leonardo Alves de Paula e Silva <leoapsilva@gmail.com>
2021-09-24 22:33:37 -0300

    first commit

---
+++ js/demo.js
@@ -0,0 +1,42 @@
+const HelloVueApp = {
+    data() {
+      return {
+        message: 'Este é o help deste campo',
+        counter: 0,
+        mostra: true,
+        nomeBotao: 'esconde',
+        newTodo: '',
+        todos: [
+            { text: 'Aprender JavaScript'},
+            { text: 'Aprender Vue' },
+            { text: 'Construir algo bacana!'},
+          ],
+        } 
+    },
+    mounted() {
+        setInterval(()=>{
+          this.counter++
+        }, 1000)
+    },
+
+    methods: {
+        reverseMessage() {
+          this.message = this.message
+            .split('')
+            .reverse()
+            .join('')
+        },
+        esconde() {
+            this.mostra = !this.mostra
+            if(this.mostra)
+                this.nomeBotao = 'esconde'
+            else
+                this.nomeBotao = 'mostra'
+        },
+        insereLista() {
+            this.todos.push(this.newTodo)
+          }
+    },
+  }
+  
+  const app = Vue.createApp(HelloVueApp)
